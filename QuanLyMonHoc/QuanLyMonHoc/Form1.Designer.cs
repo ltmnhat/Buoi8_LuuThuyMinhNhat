85d70628a99ef39c616617cf4c26f73baa9d96ca
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-10-25 16:21:22 +0700

    add file bai qlmonhoc

---
+++ QuanLyMonHoc/QuanLyMonHoc/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace QuanLyMonHoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbmamh = new System.Windows.Forms.Label();
+            this.lbtenmh = new System.Windows.Forms.Label();
+            this.txtMaMh = new System.Windows.Forms.TextBox();
+            this.txtTenMH = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbmamh
+            // 
+            this.lbmamh.AutoSize = true;
+            this.lbmamh.Location = new System.Drawing.Point(30, 45);
+            this.lbmamh.Name = "lbmamh";
+            this.lbmamh.Size = new System.Drawing.Size(42, 13);
+            this.lbmamh.TabIndex = 0;
+            this.lbmamh.Text = "Ma MH";
+            // 
+            // lbtenmh
+            // 
+            this.lbtenmh.AutoSize = true;
+            this.lbtenmh.Location = new System.Drawing.Point(30, 75);
+            this.lbtenmh.Name = "lbtenmh";
+            this.lbtenmh.Size = new System.Drawing.Size(46, 13);
+            this.lbtenmh.TabIndex = 1;
+            this.lbtenmh.Text = "Ten MH";
+            // 
+            // txtMaMh
+            // 
+            this.txtMaMh.Location = new System.Drawing.Point(92, 45);
+            this.txtMaMh.Name = "txtMaMh";
+            this.txtMaMh.Size = new System.Drawing.Size(244, 20);
+            this.txtMaMh.TabIndex = 2;
+            // 
+            // txtTenMH
+            // 
+            this.txtTenMH.Location = new System.Drawing.Point(92, 72);
+            this.txtTenMH.Name = "txtTenMH";
+            this.txtTenMH.Size = new System.Drawing.Size(244, 20);
+            this.txtTenMH.TabIndex = 3;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(36, 148);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(147, 148);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(252, 148);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 6;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 261);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtTenMH);
+            this.Controls.Add(this.txtMaMh);
+            this.Controls.Add(this.lbtenmh);
+            this.Controls.Add(this.lbmamh);
+            this.Name = "Form1";
+            this.Text = "Quan Ly Mon Hoc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbmamh;
+        private System.Windows.Forms.Label lbtenmh;
+        private System.Windows.Forms.TextBox txtMaMh;
+        private System.Windows.Forms.TextBox txtTenMH;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+    }
+}
+
---
+++ QuanLyMonHoc/QuanLyMonHoc/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyMonHoc
+{
+    public partial class Form1 : Form
+    {
+        string strketnoi=@"Data Source=A209PC02;Initial Catalog=QL_MonHoc; Integrated Security=True";
+        SqlConnection sqlketnoi = null;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                if (sqlketnoi == null)
+                {
+                    sqlketnoi = new SqlConnection(strketnoi);
+                }
+                if (sqlketnoi.State == ConnectionState.Closed)
+                {
+                    sqlketnoi.Open();
+                    MessageBox.Show("Ket noi thanh cong");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //string cmdAdd = "INSERT INTO dbo.MonHoc VALUES (1,'Lap Trinh')";
+            ////string cmdAdd = "insert into dbo.MonHoc values (" +mamh +",'"+txtTenMH.Text+"');";
+            //SqlCommand sqlcmd = new SqlCommand(cmdAdd, sqlketnoi);
+            //sqlcmd.ExecuteNonQuery();
+            string cmd = "INSERT INTO dbo.MonHoc VALUES (4,'Nguyen Nguyen')";
+            SqlCommand sqlcmd = new SqlCommand(cmd, sqlketnoi);
+            sqlcmd.ExecuteNonQuery();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string cmddelete = "delete from dbo.MonHoc where MaMH=4;";
+            SqlCommand sqlcmd = new SqlCommand(cmddelete, sqlketnoi);
+            sqlcmd.ExecuteNonQuery();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string cmdAdd = "update dbo.HocSinh set Name='Cau Truc Du Lieu Va Giai Thuat' where ID=1;";
+            SqlCommand sqlcmd = new SqlCommand(cmdAdd, sqlketnoi);
+            sqlcmd.ExecuteNonQuery();
+        }
+    }
+}
